0f58068386eb6a7c614951028cb86c2a9b60ee8a
legendarypirate <bayka.lkh@gmail.com>
2026-08-29 19:14:16 +0800

    ss

--- menbackend/src/server.js
+++ menbackend/src/server.js
@@ -1,19 +1,11 @@
 const { envPath, port } = require('./config/env');
 const app = require('./app');
 const { sequelize } = require('./models');
-const { ensureWorkoutPrograms } = require('./bootstrap/ensureWorkoutPrograms');
-const { ensureContent } = require('./bootstrap/ensureContent');
-const { ensureQuiz } = require('./bootstrap/ensureQuiz');
 
 async function start() {
   try {
     await sequelize.authenticate();
     await sequelize.sync({ alter: process.env.NODE_ENV !== 'production' });
-    await ensureWorkoutPrograms();
-    await ensureContent();
-    await ensureQuiz();
-    const { ensureDemoUser } = require('./bootstrap/ensureAuth');
-    await ensureDemoUser();
     const { startReminderScheduler } = require('./jobs/reminderScheduler');
     startReminderScheduler();
     console.log('Database connected');
